54eed781aa23d23f4d72ef5219cb1c0725f020b1
kasramp <kasra_mp@yahoo.com>
2017-10-08 00:42:34 +0800

    Add unit test for conversion util and minor code clean up

--- src/test/java/com/madadipouya/eris/util/UnitConversionUtilsTest.java
+++ src/test/java/com/madadipouya/eris/util/UnitConversionUtilsTest.java
@@ -1,14 +1,42 @@
 package com.madadipouya.eris.util;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import static com.madadipouya.eris.util.UnitConversionUtils.*;
 import static junit.framework.TestCase.assertEquals;
 
+@RunWith(MockitoJUnitRunner.class)
 public class UnitConversionUtilsTest {
 
     @Test
     public void testFahrenheitToCelsius() {
-        assertEquals(UnitConversionUtils.FahrenheitToCelsius(104), 40, 0.001);
+        assertEquals(FahrenheitToCelsius(104), 40, 0.0001);
     }
-    
+
+    @Test
+    public void testCelsiusToFahrenheit() {
+        assertEquals(CelsiusToFahrenheit(40), 104, 0.0001);
+    }
+
+    @Test
+    public void testKiloMeterPerHourToMilePerHour() {
+        assertEquals(KiloMeterPerHourToMilePerHour(100), 62.1371, 0.0001);
+    }
+
+    @Test
+    public void testMeterToMile() {
+        assertEquals(MeterToMile(100), 0.0621371, 0.0000001);
+    }
+
+    @Test
+    public void testMeterPerSecondToKiloMeterPerHour() {
+        assertEquals(MeterPerSecondToKiloMeterPerHour(100), 360, 0.0001);
+    }
+
+    @Test
+    public void testMeterToKiloMeter() {
+        assertEquals(MeterToKiloMeter(1000), 1, 0.0001);
+    }
 }
