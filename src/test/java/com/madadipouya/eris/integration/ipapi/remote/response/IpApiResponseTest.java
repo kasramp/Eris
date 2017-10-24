15e483b05454b3430400ca736e4a4d8137507b05
kasramp <kasra_mp@yahoo.com>
2017-10-24 21:42:19 +0800

    Add license header

--- src/test/java/com/madadipouya/eris/integration/ipapi/remote/response/IpApiResponseTest.java
+++ src/test/java/com/madadipouya/eris/integration/ipapi/remote/response/IpApiResponseTest.java
@@ -6,9 +6,23 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by kixz on 10/24/17.
- */
+/*
+* This file is part of Eris Weather API.
+*
+* Eris Weather API is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3
+* as published by the Free Software Foundation.
+*
+* Eris Weather API is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.  <http://www.gnu.org/licenses/>
+*
+* Author(s):
+*
+* © 2017 Kasra Madadipouya <kasra@madadipouya.com>
+*/
+
 @RunWith(MockitoJUnitRunner.class)
 public class IpApiResponseTest {
 
