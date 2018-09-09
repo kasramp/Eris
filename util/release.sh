f309a21fd7e7d4653e990af99b0a9fd0f7e6a86b
kasramp <kasra_mp@yahoo.com>
2018-09-09 23:36:57 +0200

    Avoid stucking in release for-loop due to Maven plugin commit

--- util/release.sh
+++ util/release.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -ev
 
-git config --global user.email $GITHUB_EMAIL
-git config --global user.name $GITHUB_USERNAME
-git remote set-head origin $TRAVIS_BRANCH
-git show-ref --head
-git symbolic-ref HEAD refs/heads/$TRAVIS_BRANCH
-git symbolic-ref HEAD
-mvn -B clean org.jacoco:jacoco-maven-plugin:prepare-agent release:clean release:prepare -Dusername=$GITHUB_USERNAME -Dpassword=$GITHUB_PASSWORD sonar:sonar
-mvn -B release:perform -Dusername=$GITHUB_USERNAME -Dpassword=$GITHUB_PASSWORD
+if [[ "$TRAVIS_COMMIT_DESCRIPTION" != *"maven-release-plugin"* ]];then
+    git config --global user.email $GITHUB_EMAIL
+    git config --global user.name $GITHUB_USERNAME
+    git remote set-head origin $TRAVIS_BRANCH
+    git show-ref --head
+    git symbolic-ref HEAD refs/heads/$TRAVIS_BRANCH
+    git symbolic-ref HEAD
+    mvn -B clean org.jacoco:jacoco-maven-plugin:prepare-agent release:clean release:prepare -Dusername=$GITHUB_USERNAME -Dpassword=$GITHUB_PASSWORD sonar:sonar
+fi
