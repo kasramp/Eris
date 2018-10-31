75fee60fba74ca87f4bf9d74a3a46ed31b307aca
Kasra Madadipouya <kasra_mp@yahoo.com>
2018-10-31 21:02:11 +0100

    Update release.sh

--- util/release.sh
+++ util/release.sh
@@ -8,5 +8,5 @@ if [[ "$TRAVIS_COMMIT_DESCRIPTION" != *"maven-release-plugin"* ]];then
     git show-ref --head
     git symbolic-ref HEAD refs/heads/$TRAVIS_BRANCH
     git symbolic-ref HEAD
-    mvn -B clean release:clean release:prepare -Dusername=$GITHUB_USERNAME -Dpassword=$GITHUB_PASSWORD
+    mvn -B clean release:clean release:prepare release:perform -Dusername=$GITHUB_USERNAME -Dpassword=$GITHUB_PASSWORD
 fi
