b91067bc78cdc9efbe5ac2e702b2b3dc0f3ae3fd
kasramp <kasra_mp@yahoo.com>
2021-12-15 16:40:02 +0100

    Minor clean up

--- util/release.sh
+++ util/release.sh
@@ -4,8 +4,6 @@ set -ev
 if [[ "$LAST_COMMIT" != *"maven-release-plugin"* ]];then
     git config --global user.email $EMAIL
     git config --global user.name $USERNAME
-#    git remote set-head origin develop
-#    git show-ref --head
     git symbolic-ref HEAD refs/heads/develop
     git symbolic-ref HEAD
     mvn -B clean release:clean release:prepare release:perform -Dusername=$USERNAME -Dpassword=$TOKEN -Darguments="-Dmaven.deploy.skip=true"
