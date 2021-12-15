03a10e35a37a8dd9c1968562c0befcf5d4308fca
kasramp <kasra_mp@yahoo.com>
2021-12-15 16:18:00 +0100

    Don't deploy

--- util/release.sh
+++ util/release.sh
@@ -6,7 +6,7 @@ if [[ "$LAST_COMMIT" != *"maven-release-plugin"* ]];then
     git config --global user.name $USERNAME
 #    git remote set-head origin develop
 #    git show-ref --head
-#    git symbolic-ref HEAD refs/heads/develop
-#    git symbolic-ref HEAD
-    mvn -B clean release:clean release:prepare release:perform -Dusername=$USERNAME -Dpassword=$TOKEN
+    git symbolic-ref HEAD refs/heads/develop
+    git symbolic-ref HEAD
+    mvn -B clean release:clean release:prepare release:perform -Dusername=$USERNAME -Dpassword=$TOKEN -Darguments="-Dmaven.deploy.skip=true"
 fi
