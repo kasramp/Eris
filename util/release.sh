519d2fe540ac672095d9cdefe8a1f90a62e5d2e7
kasramp <kasra_mp@yahoo.com>
2021-12-15 15:02:36 +0100

    Modify release script

--- util/release.sh
+++ util/release.sh
@@ -4,9 +4,9 @@ set -ev
 if [[ "$LAST_COMMIT" != *"maven-release-plugin"* ]];then
     git config --global user.email $EMAIL
     git config --global user.name $USERNAME
-    git remote set-head origin develop
-    git show-ref --head
-    git symbolic-ref HEAD refs/heads/develop
-    git symbolic-ref HEAD
+#    git remote set-head origin develop
+#    git show-ref --head
+#    git symbolic-ref HEAD refs/heads/develop
+#    git symbolic-ref HEAD
     mvn -B clean release:clean release:prepare release:perform -Dusername=$USERNAME -Dpassword=$TOKEN
 fi
