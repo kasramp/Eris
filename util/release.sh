73e3d5bebbe40008852cb8cf03ebc570f78869d8
kasramp <kasra_mp@yahoo.com>
2021-12-15 14:40:46 +0100

    Move to GitHub actions

--- util/release.sh
+++ util/release.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -ev
 
-if [[ "$TRAVIS_COMMIT_DESCRIPTION" != *"maven-release-plugin"* ]];then
-    git config --global user.email $GITHUB_EMAIL
-    git config --global user.name $GITHUB_USERNAME
-    git remote set-head origin $TRAVIS_BRANCH
+if [[ "$LAST_COMMIT" != *"maven-release-plugin"* ]];then
+    git config --global user.email $EMAIL
+    git config --global user.name $USERNAME
+    git remote set-head origin develop
     git show-ref --head
-    git symbolic-ref HEAD refs/heads/$TRAVIS_BRANCH
+    git symbolic-ref HEAD refs/heads/develop
     git symbolic-ref HEAD
-    mvn -B clean release:clean release:prepare release:perform -Dusername=$GITHUB_USERNAME -Dpassword=$GITHUB_PASSWORD
+    mvn -B clean release:clean release:prepare release:perform -Dusername=$USERNAME -Dpassword=$TOKEN
 fi
