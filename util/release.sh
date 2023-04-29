661bc5335674a5798307848411cd2f77fe908f1b
kasramp <kasra_mp@yahoo.com>
2023-04-29 09:12:21 +0200

    #25 tags from develop branch should be marked as beta

--- util/release.sh
+++ util/release.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -ev
 
-if [[ "$LAST_COMMIT" != *"maven-release-plugin"* ]];then
+if [ "$LAST_COMMIT" != *"maven-release-plugin"* ]; then
     git config --global user.email $EMAIL
     git config --global user.name $USERNAME
-    git symbolic-ref HEAD refs/heads/develop
+    git symbolic-ref HEAD refs/heads/$(git branch --show-current)
     git symbolic-ref HEAD
-    mvn -B clean release:clean release:prepare release:perform -Dusername=$USERNAME -Dpassword=$TOKEN -Darguments="-Dmaven.deploy.skip=true"
+    if [ $(git branch --show-current) == "develop" ]; then
+      mvn -B clean release:clean release:prepare release:perform -Dusername=$USERNAME -Dpassword=$TOKEN -DtagNameFormat=${artifactId}-BETA-${version} -Darguments="-Dmaven.deploy.skip=true"
+    else
+      mvn -B clean release:clean release:prepare release:perform -Dusername=$USERNAME -Dpassword=$TOKEN -Darguments="-Dmaven.deploy.skip=true"
+    fi
 fi
