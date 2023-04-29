26ef04ab9e48286c1ed4c00fdda2496ae1cedfd9
kasramp <kasra_mp@yahoo.com>
2023-04-29 10:37:34 +0200

    #25 use '@' instead of '$' as per documentation recommendation

--- util/release.sh
+++ util/release.sh
@@ -7,7 +7,7 @@ if [ "$LAST_COMMIT" != *"maven-release-plugin"* ]; then
     git symbolic-ref HEAD refs/heads/$(git branch --show-current)
     git symbolic-ref HEAD
     if [ $(git branch --show-current) == "develop" ]; then
-      mvn -B clean release:clean release:prepare release:perform -Dusername=$USERNAME -Dpassword=$TOKEN -DtagNameFormat='${artifactId}-beta-${version}' -Darguments="-Dmaven.deploy.skip=true"
+      mvn -B clean release:clean release:prepare release:perform -Dusername=$USERNAME -Dpassword=$TOKEN -DtagNameFormat=@{artifactId}-beta-@{version} -Darguments="-Dmaven.deploy.skip=true"
     else
       mvn -B clean release:clean release:prepare release:perform -Dusername=$USERNAME -Dpassword=$TOKEN -Darguments="-Dmaven.deploy.skip=true"
     fi
