54649861acf854f5b9d212012ceaee9d656349a4
kasramp <kasra_mp@yahoo.com>
2023-05-03 00:45:06 +0200

    #25 build image after tagging a version

--- util/release.sh
+++ util/release.sh
@@ -10,6 +10,9 @@ if [[ "$LAST_COMMIT" != *"maven-release-plugin"* ]]; then
       mvn -B clean release:clean release:prepare release:perform -Dusername=$USERNAME -Dpassword=$TOKEN -DtagNameFormat=@{artifactId}-beta-@{version} -Darguments="-Dmaven.deploy.skip=true"
     else
       mvn -B clean release:clean release:prepare release:perform -Dusername=$USERNAME -Dpassword=$TOKEN -Darguments="-Dmaven.deploy.skip=true" &&
+      mvn clean spring-boot:build-image &&
+      TAGGED_VERSION=$(git for-each-ref --sort=-creatordate --format='%(refname:short)' refs/tags --count 10 | grep -oP '^eris-(\d+).*' | head -1 | cut -d "-" -f2) &&
+      docker tag eris:"$TAGGED_VERSION" eris:latest &&
       git fetch origin develop && git merge origin/master &&
       git push https://$USERNAME:$TOKEN@github.com/kasramp/Eris.git HEAD:develop
     fi
